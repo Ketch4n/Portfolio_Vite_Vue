0834d52d71e0edc0fed9933204fe15c71b505e5e
Ketch4n <mangao.christian.04@gmail.com>
2024-11-08 17:16:31 +0800

    Navbar-Initialize

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+  </div>
+</template>
+
+<script setup>
+  import { defineAsyncComponent } from 'vue';
+  const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+</script>
